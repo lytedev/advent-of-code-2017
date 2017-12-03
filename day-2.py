f476e202d3327b5bcf0794c66ad18c3c63ff25e8
Daniel Flanagan <daniel@lytedev.io>
2017-12-03 10:46:33 -0600

    Add Day 2

---
+++ day-2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# usage: cat inputs/day-2.txt | ./$SCRIPTNAME.py
+
+import sys
+
+def spreadsheet_line(line, sep="\t"):
+    highest = -sys.maxsize
+    lowest = sys.maxsize
+    numbers = map(int, line.split(sep))
+    try:
+        for x in numbers:
+            if x < lowest:
+                lowest = x
+            if x > highest:
+                highest = x
+    except:
+        return
+
+    return highest - lowest
+
+def checksum(spreadsheet_text, line_sep="\n"):
+    total = 0
+    lines = spreadsheet_text.split(line_sep)
+    for line in lines:
+        line_diff = spreadsheet_line(line)
+        try:
+            total += line_diff
+        except:
+            pass
+    return total
+
+if __name__ == "__main__":
+    print(checksum(sys.stdin.read()))
+
